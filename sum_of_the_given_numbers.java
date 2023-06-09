2cd560c090e4a9a48830e991eedcbc1bbec4474a
Reddy Hemanth <102216340+21MH5A0427@users.noreply.github.com>
2023-06-09 17:17:33 +0530

    import java.util.*; public class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int t;         t=sc.nextInt();         for(int i=0;i<t;i++)         {             int a,b,c;             a=sc.nextInt();             b=sc.nextInt();             c=a+b;             System.out.println(c);         }         sc.close();     } }

---
+++ sum_of_the_given_numbers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t;
+        t=sc.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int a,b,c;
+            a=sc.nextInt();
+            b=sc.nextInt();
+            c=a+b;
+            System.out.println(c);
+        }
+        sc.close();
+    }
+}
